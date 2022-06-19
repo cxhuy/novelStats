4d0e712a956b8185b0b73759005edd156997947a
cxhuy <alvinkang1@gmail.com>
2022-06-19 17:20:29 +0900

    now doesn’t print novels at initial run

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 url = 'https://novel.munpia.com/page/novelous/group/nv.regular/gpage/1'
 lastNovelId = -1
+initialRun = True
 
 def getSoup(url):
     response = requests.get(url)
@@ -17,22 +18,28 @@ def getSoup(url):
 
 def printNewNovels():
     print(datetime.now())
-    global lastNovelId
+    global lastNovelId, initialRun
     newNovels = []
     novelList = getSoup(url).find(id="SECTION-LIST").select('li')
 
-    for i in range(len(novelList)):
-        novel = {}
-        currentNovel = novelList[i]
-        novel["id"] = int(currentNovel.find(class_="title").get('href').split('https://novel.munpia.com/')[-1])
-        if (novel["id"] == lastNovelId): break
-        novel["title"] = currentNovel.find(class_="title").text.strip()
-        novel["author"] = currentNovel.find(class_="author").text.strip()
-        print("id: " + str(novel["id"]) + "\ntitle: " + novel["title"] + "\nauthor: " + novel["author"] + "\n")
-        newNovels.append(novel)
-    if (len(newNovels) > 0): lastNovelId = newNovels[0]["id"]
-    print(lastNovelId)
-    print(newNovels)
+    if (initialRun == True):
+        lastNovelId = int(novelList[0].find(class_="title").get('href').split('https://novel.munpia.com/')[-1])
+        initialRun = False
+
+    else:
+        for i in range(len(novelList)):
+            novel = {}
+            currentNovel = novelList[i]
+            novel["id"] = int(currentNovel.find(class_="title").get('href').split('https://novel.munpia.com/')[-1])
+            if (novel["id"] == lastNovelId): break
+            novel["title"] = currentNovel.find(class_="title").text.strip()
+            novel["author"] = currentNovel.find(class_="author").text.strip()
+            print("id: " + str(novel["id"]) + "\ntitle: " + novel["title"] + "\nauthor: " + novel["author"] + "\n")
+            newNovels.append(novel)
+        if (len(newNovels) > 0): lastNovelId = newNovels[0]["id"]
+
+    # print(lastNovelId)
+    print("none" if len(newNovels) == 0 else newNovels, end='\n')
 
 print("started script at " + str(datetime.now()) + "\n")
 
