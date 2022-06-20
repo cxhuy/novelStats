27ea93942e496c9198b0d51331fb175066d618c6
cxhuy <alvinkang1@gmail.com>
2022-06-20 15:46:30 +0900

    now crawls each novel’s details after an hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests, schedule, time
 from bs4 import BeautifulSoup
-from datetime import datetime
+from datetime import datetime, timedelta
 
 url = 'https://novel.munpia.com/page/novelous/group/nv.regular/gpage/1'
 lastNovelId = -1
@@ -19,8 +19,18 @@ def getSoup(url):
 def extractVal(dd):
     return int(dd.text.split(' ')[0].replace(',', ''))
 
+def checkLater(novel):
+    novelUrl = 'https://novel.munpia.com/' + str(novel["id"])
+    currentTime = datetime.now()
+    novelDetails = getSoup(novelUrl).find(class_="detail-box").select('dl')[-1].select('dd')
+    novel["end_views"] = extractVal(novelDetails[1])
+    novel["end_likes"] = extractVal(novelDetails[2])
+    novel["end_time"] = currentTime
+    print(novel)
+    return schedule.CancelJob
+
 def printNewNovels():
-    print(datetime.now())
+    print('\n' + str(datetime.now()) + "\n[New Novels]")
     global lastNovelId, initialRun
     newNovels = []
     novelList = getSoup(url).find(id="SECTION-LIST").select('li')
@@ -37,27 +47,32 @@ def printNewNovels():
             if (novel["id"] == lastNovelId): break
             novel["title"] = currentNovel.find(class_="title").text.strip()
             novel["author"] = currentNovel.find(class_="author").text.strip()
-
             novelUrl = 'https://novel.munpia.com/' + str(novel["id"])
+            currentTime = datetime.now()
             novelDetails = getSoup(novelUrl).find(class_="detail-box").select('dl')[-1].select('dd')
             novel["chapters"] = extractVal(novelDetails[0])
             novel["letters"] = extractVal(novelDetails[3])
             novel["start_views"] = extractVal(novelDetails[1])
-            novel["finish_views"] = -1
+            novel["end_views"] = -1
             novel["start_likes"] = extractVal(novelDetails[2])
-            novel["finish_likes"] = -1
+            novel["end_likes"] = -1
+            novel["start_time"] = currentTime
+            novel["end_time"] = -1
             # print("id: " + str(novel["id"]) + "\ntitle: " + novel["title"] + "\nauthor: " + novel["author"] + "\n")
             newNovels.append(novel)
+            laterTime = currentTime + timedelta(hours=1)
+            laterTime = str(laterTime.hour).rjust(2, '0') + ':' + str(laterTime.minute).rjust(2, '0')
+            schedule.every().day.at(laterTime).do(checkLater, novel)
         if (len(newNovels) > 0): lastNovelId = newNovels[0]["id"]
 
     # print(lastNovelId)
     if (len(newNovels) == 0):
-        print("none\n")
+        print("none")
     else:
         for novelToPrint in newNovels:
             print(novelToPrint)
-        print()
-    # print("none" if len(newNovels) == 0 else newNovels, end='\n')
+    print("[Old Novels]")
+
 
 print("started script at " + str(datetime.now()) + "\n")
 
