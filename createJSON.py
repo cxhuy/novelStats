57a7daf53e3b4e473d6013584d6d4e27e67fcc59
cxhuy <alvinkang1@gmail.com>
2022-08-01 18:44:28 +0900

    change cursor to dictcursor

--- createJSON.py
+++ createJSON.py
@@ -1,4 +1,5 @@
 import json, os, pymysql
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,7 +12,7 @@ conn = pymysql.connect(
     charset='utf8'
 )
 
-cur = conn.cursor()
+cur = pymysql.cursors.DictCursor(conn)
 
 munpiaData = {
     'platformInfoData': {
@@ -48,26 +49,43 @@ munpiaData = {
 
 # platforms = ['munpia', 'novelpia', 'kakaopage', 'kakaostage', 'navernovel']
 platforms = ['munpia']
-
+a = []
 for platform in platforms:
     sql = "select * from extendednovelData where novelInstanceId = maxNovelInstanceIId and platform = '" + platform + "' and start_time >= subdate(current_timestamp, 7);"
     cur.execute(sql)
+    rows = cur.fetchall()
+
     total_views = 0
     total_novels = 0
     total_chapters = 0
     total_upload_periods = 0
-    for row in cur:
-        if (row[12] != None):
-            total_views += row[12]
+
+    for row in rows:
+        if (row["end_total_views"] != None):
+            total_views += row["end_total_views"]
         total_novels += 1
-        total_chapters += row[6]
-        total_upload_periods += row[-1]
+        total_chapters += row["chapters"]
+        total_upload_periods += row["weeklyUploadCount"]
+
     munpiaData['platformInfoData']['totalViews'] = total_views
     munpiaData['platformInfoData']['totalNovels'] = total_novels
     munpiaData['platformInfoData']['avgViews'] = int(total_views / total_novels)
     munpiaData['platformInfoData']['avgChapters'] = int(total_chapters / total_novels)
+
+    current_time = datetime.today()
+    for weekday in range(7):
+        print(weekday)
+        for row in rows:
+            if (row["start_time"].day == (current_time - timedelta(days=current_time.weekday())).day or current_time.weekday() == weekday):
+                print(row["start_time"])
+
     # print(platform, total_novels, total_views, total_views/total_novels, total_chapters/total_novels, total_upload_periods/total_novels)
 
 print(json.dumps(munpiaData, indent=4, sort_keys=True))
+# print(datetime.now().weekday() - timedelta(days=current_time.weekday())).day)
+
+date = []
+date_time_str = '2022-07-31 01:14:22'
+print(datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S').day)
 
 conn.commit()
