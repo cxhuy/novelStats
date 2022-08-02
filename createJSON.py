f0b41ffb3873aec1b3885c75c1bcea2190253450
cxhuy <alvinkang1@gmail.com>
2022-08-02 23:45:42 +0900

    finish heatmapData by genre

--- createJSON.py
+++ createJSON.py
@@ -19,7 +19,7 @@ munpiaData = {
         'platformName': "문피아",
         'platformYear': 2012,
         'platformMonth': 12,
-        'platformGenres': ["무협", "판타지", "퓨전", "게임", "스포츠", "로맨스", "라이트노벨", "현대판타지", "대체역사", "전쟁·밀리터리", "SF", "추리",
+        'platformGenres': ["모든 장르", "무협", "판타지", "퓨전", "게임", "스포츠", "로맨스", "라이트노벨", "현대판타지", "대체역사", "전쟁·밀리터리", "SF", "추리",
                            "공포·미스테리", "일반소설", "시·수필", "중·단편", "아동소설·동화", "드라마", "연극·시나리오", "BL", "팬픽·패러디"],
         'platformMonopoly': ["비독점", "선독점", "독점"],
         'platformPricings': ["무료 작가연재", "무료 일반연재", "유료 연재작"],
@@ -29,13 +29,7 @@ munpiaData = {
         'avgChapters': -1,
     },
 
-    'heatmapData': {
-        'views': {},
-        'mostViews': -1,
-        'uploads': {},
-        'mostUploads': -1,
-        'bestTimes': [],
-    },
+    'heatmapData': {},
 
     'genreData': {},
 
@@ -64,12 +58,20 @@ for platform in platforms:
     total_novels = 0
     total_chapters = 0
 
-    for i in range(7):
-        eval(platform + "Data")["heatmapData"]["views"][i] = {}
-        eval(platform + "Data")["heatmapData"]["uploads"][i] = {}
-        for j in range(24):
-            eval(platform + "Data")["heatmapData"]["views"][i][j] = 0
-            eval(platform + "Data")["heatmapData"]["uploads"][i][j] = 0
+    for platformGenre in eval(platform + "Data")["platformInfoData"]["platformGenres"]:
+        eval(platform + "Data")["heatmapData"][platformGenre] = {
+            'views': {},
+            'mostViews': -1,
+            'uploads': {},
+            'mostUploads': -1,
+            'bestTimes': [],
+        }
+        for i in range(7):
+            eval(platform + "Data")["heatmapData"][platformGenre]["views"][i] = {}
+            eval(platform + "Data")["heatmapData"][platformGenre]["uploads"][i] = {}
+            for j in range(24):
+                eval(platform + "Data")["heatmapData"][platformGenre]["views"][i][j] = 0
+                eval(platform + "Data")["heatmapData"][platformGenre]["uploads"][i][j] = 0
 
     for genre in eval(platform + "Data")["platformInfoData"]["platformGenres"]:
         eval(platform + "Data")["genreData"][genre] = {
@@ -115,22 +117,12 @@ for platform in platforms:
             'avgFavs': 0,
         }
 
-    # sql = "select genre from genres where novelInstanceId = maxNovelInstanceIId and platform = '" + platform + "' and start_time >= subdate(current_timestamp, 7);"
-    # cur.execute(sql)
-    # rows = cur.fetchall()
-    # conn.commit()
-
     dataTypes = ["monopoly", "pricing", "weeklyUploadCount"]
 
     for row in rows:
         total_views += max(row["start_total_views"], row["end_total_views"])
         total_novels += 1
         total_chapters += row["chapters"]
-
-        if (row["end_total_views"] > -1):
-            eval(platform + "Data")["heatmapData"]["views"][row["start_time"].weekday()][row["start_time"].hour] += \
-                row["end_total_views"] - row["start_total_views"] if row["end_total_views"] - row["start_total_views"] > 0 else 0
-            eval(platform + "Data")["heatmapData"]["uploads"][row["start_time"].weekday()][row["start_time"].hour] += 1
 
         sql = "select genre from genres where novelInstanceId=%s"
         cur.execute(sql, (row["novelInstanceId"]))
@@ -139,6 +131,21 @@ for platform in platforms:
 
         for rowGenre in rowGenres:
             rowGenre = rowGenre["genre"]
+
+            if (row["end_total_views"] > -1):
+                eval(platform + "Data")["heatmapData"]["모든 장르"]["views"][row["start_time"].weekday()][
+                    row["start_time"].hour] += \
+                    row["end_total_views"] - row["start_total_views"] if row["end_total_views"] - row[
+                        "start_total_views"] > 0 else 0
+                eval(platform + "Data")["heatmapData"]["모든 장르"]["uploads"][row["start_time"].weekday()][
+                    row["start_time"].hour] += 1
+
+                eval(platform + "Data")["heatmapData"][rowGenre]["views"][row["start_time"].weekday()][row["start_time"].hour] += \
+                    row["end_total_views"] - row["start_total_views"] if row["end_total_views"] - row[
+                        "start_total_views"] > 0 else 0
+                eval(platform + "Data")["heatmapData"][rowGenre]["uploads"][row["start_time"].weekday()][
+                    row["start_time"].hour] += 1
+
             eval(platform + "Data")["genreData"][rowGenre]["novelCount"] += 1
 
             if (platform in ["munpia", "novelpia", "kakaostage", "kakaopage"]):
@@ -186,19 +193,22 @@ for platform in platforms:
     eval(platform + "Data")["platformInfoData"]["avgViews"] = int(total_views / total_novels)
     eval(platform + "Data")["platformInfoData"]["avgChapters"] = int(total_chapters / total_novels)
 
-    viewList = []
-    uploadList = []
-    for i in range(7):
-        viewList += list(eval(platform + "Data")["heatmapData"]["views"][i].values())
-        uploadList += list(eval(platform + "Data")["heatmapData"]["uploads"][i].values())
-    eval(platform + "Data")["heatmapData"]["mostViews"] = max(viewList)
-    eval(platform + "Data")["heatmapData"]["mostUploads"] = max(uploadList)
+    for platformGenre in eval(platform + "Data")["platformInfoData"]["platformGenres"]:
+        viewList = []
+        uploadList = []
+        for i in range(7):
+            viewList += list(eval(platform + "Data")["heatmapData"][platformGenre]["views"][i].values())
+            uploadList += list(eval(platform + "Data")["heatmapData"][platformGenre]["uploads"][i].values())
+        eval(platform + "Data")["heatmapData"][platformGenre]["mostViews"] = max(viewList)
+        eval(platform + "Data")["heatmapData"][platformGenre]["mostUploads"] = max(uploadList)
 
-    avgViewList = [int(viewList[x]/uploadList[x]) if uploadList[x] != 0 else 0 for x in range(7*24)]
-    for i in range(5):
-        index = avgViewList.index(max(avgViewList))
-        eval(platform + "Data")["heatmapData"]["bestTimes"].append(["월", "화", "수", "목", "금", "토", "일"][int(index/24)] + " " + str(index%24).rjust(2, '0') + ":00 ~ " + str(index%24 + 1).rjust(2, '0') + ":00")
-        avgViewList[index] = 0
+        avgViewList = [int(viewList[x]/uploadList[x]) if uploadList[x] != 0 else 0 for x in range(7*24)]
+        for i in range(5):
+            if (max(avgViewList) == 0):
+                break
+            index = avgViewList.index(max(avgViewList))
+            eval(platform + "Data")["heatmapData"][platformGenre]["bestTimes"].append(["월", "화", "수", "목", "금", "토", "일"][int(index/24)] + " " + str(index%24).rjust(2, '0') + ":00 ~ " + str(index%24 + 1).rjust(2, '0') + ":00")
+            avgViewList[index] = 0
 
 print(json.dumps(munpiaData, indent=4, sort_keys=True))
 
